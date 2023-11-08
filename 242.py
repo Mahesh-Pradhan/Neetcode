56a10baf9bd396a9999f16f5c43fa58a2952e9f1
Mahesh-Pradhan <Dk482389@gmail.com>
2023-11-08 20:25:19 +0530

    Valid Anagram

---
+++ 242.py
@@ -0,0 +1,21 @@
+# 242. Valid Anagram
+
+def isanagram1(s, t): # in this approach we are counting the number of 
+                            # each char in both string and then compare both the string.
+    if len(s)!=len(t):
+        return False
+    counts1,counts2={},{}             # we will using hashmap to count and store the char count
+    for i in range(len(s)):
+        counts1[s[i]] = 1 + counts1.get(s[i],0)
+        counts2[t[i]] = 1 + counts2.get(t[i],0)
+    
+    for c in counts1:
+        if counts1[c]!=counts2.get(c,0):
+            return False
+    return True
+
+def isAnagram2(s,t):# I this, we used a sorted() func which sorts strg and converts it 
+    return sorted(s)==sorted(t)# to a list of chars in the string.
+
+def isAnagram(s,t):# Counter acts as a hashmap, which counts all the occurance of char in the strg 
+    return Counter(s)== Counter(t)
