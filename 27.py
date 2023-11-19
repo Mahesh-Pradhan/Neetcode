4fec1e29f54ea118316fde8040c0c460c4a12a9d
Mahesh-Pradhan <Dk482389@gmail.com>
2023-11-19 08:09:44 +0530

    27. Remove Element

---
+++ 27.py
@@ -0,0 +1,10 @@
+# 27. Remove Element
+
+def removeElements(nums,val):
+
+    k=0
+    for i in range(len(nums)):
+        if nums[i]!=val:
+            nums[k]=nums[i]
+            k+=1
+    return k
