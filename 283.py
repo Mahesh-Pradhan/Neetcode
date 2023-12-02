e3ab0f287814689c19d6acf9bc7a4765b665d7a5
Mahesh-Pradhan <Dk482389@gmail.com>
2023-12-02 12:15:02 +0530

    283. Move Zeroes

---
+++ 283.py
@@ -0,0 +1,11 @@
+# 283. Move Zeroes
+
+def moveZeroes(nums):
+    i,j=0,0
+
+    while j<len(nums):
+        if nums[j]!=0:
+            nums[i],nums[j]=nums[j],nums[i]
+            i+=1
+        j+=1
+    
