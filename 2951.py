7e2626c5105bed38c14d12bb7f89ee6878598094
Mahesh-Pradhan <Dk482389@gmail.com>
2023-12-05 23:23:26 +0530

    2951. Find the Peaks

---
+++ 2951.py
@@ -0,0 +1,7 @@
+# 2951. Find the Peaks
+def findPeaks( mountain):
+    result=[]
+    for i in range(1,len(mountain)-1):
+        if mountain[i-1]<mountain[i] > mountain[i+1]:
+            result.append(i)
+    return result
