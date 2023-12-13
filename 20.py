40129d8ebaede603d3d831ff3038f1e08babe4a5
Mahesh-Pradhan <Dk482389@gmail.com>
2023-12-13 14:00:43 +0530

    20. Valid Parentheses

---
+++ 20.py
@@ -0,0 +1,19 @@
+# 20. Valid Parentheses
+
+def isValid(s):
+    stack=[]
+    closeToopen={")":"(", "]":"[", "}":"{"}
+
+    for i in s:
+
+        if i in closeToopen:
+            if stack and stack[-1]==closeToopen[i]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(i)
+
+    return True if not stack else False
+
+
